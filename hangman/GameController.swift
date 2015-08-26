c9d4bb5eb4c4b58ea0ca47df013c289a731fb780
aem <amarkon895@gmail.com>
2015-08-26 12:42:59 -0400

    Added some fancy new user prompts to change game modes and input words

--- hangman/GameController.swift
+++ hangman/GameController.swift
@@ -12,7 +12,21 @@ public struct GameController {
     private let board: GameBoard
     
     init() {
-        self.board = GameBoard(customWord: "poop")
+        print("Would you like to enter a custom word for someone else to play? (y/n)")
+        var answer = InputCollector.getInputCharacter()
+        while !Y_N_INPUTS.contains(answer) {
+            print("Please choose 'y' or 'n'")
+            answer = InputCollector.getInputCharacter()
+        }
+        
+        if Y_INPUTS.contains(answer) {
+            print("Please input the word that you'd like someone else to guess!")
+            let word = InputCollector.getInputWord()
+            self.board = GameBoard(customWord: word)
+        } else {
+            print("You're going to be guessing a random word, good luck!")
+            self.board = GameBoard()
+        }
     }
     
     public func play() -> Int {
--- hangman/constants.swift
+++ hangman/constants.swift
@@ -8,4 +8,8 @@
 
 import Foundation
 
-let HANGMAN_BOARD = "   ------------- \n   |           | \n               | \n               | \n               | \n               | \n               | \n               | \n               | \n   ____________|_"
+let HANGMAN_BOARD: String = "   ------------- \n   |           | \n               | \n               | \n               | \n               | \n               | \n               | \n               | \n   ____________|_"
+
+let Y_N_INPUTS: Array<Character> = [Character("y"), Character("Y"), Character("n"), Character("N")]
+let Y_INPUTS: Array<Character> = [Character("y"), Character("Y")]
+let N_INPUTS: Array<Character> = [Character("n"), Character("N")]
